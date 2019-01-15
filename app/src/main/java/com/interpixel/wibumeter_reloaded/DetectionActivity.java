02d8edecd37d22326ed70b20b7779b53fdaee6b2
Stanley Heryanto <stanleyheryanto@Gmail.com>
2019-01-16 05:55:29 +0700

    Maybe detects?

--- app/src/main/java/com/interpixel/wibumeter_reloaded/DetectionActivity.java
+++ app/src/main/java/com/interpixel/wibumeter_reloaded/DetectionActivity.java
@@ -9,6 +9,7 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.ImageFormat;
 import android.graphics.Rect;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
@@ -17,6 +18,7 @@ import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
 import android.media.Image;
+import android.media.ImageReader;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -37,6 +39,7 @@ import com.google.firebase.ml.vision.face.FirebaseVisionFace;
 import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
 import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -47,7 +50,9 @@ public class DetectionActivity extends AppCompatActivity {
 
     private FirebaseVisionFaceDetector detector;
     private FirebaseVisionImage image;
-    private Surface cameraSurface;
+    private ImageReader imageReader;
+    private Surface detectionSurface;
+    private Surface previewSurface;
     private SurfaceView surfaceView;
     private SurfaceHolder surfaceHolder;
     private SurfaceHolder.Callback surfaceHolderCallback;
@@ -55,10 +60,13 @@ public class DetectionActivity extends AppCompatActivity {
     private CameraDevice cameraDevice;
     private CameraDevice.StateCallback cameraStateCallback;
     private CameraCaptureSession captureSession;
-    private String[] cameraId;  //kamera depan atau belakang (atau samping wkwk)
+    private String[] cameraIds;  //kamera depan atau belakang (atau samping wkwk)
+    private String cameraId;
     private boolean surfaceReady = false;
     private boolean isPreviewing = false;
     private boolean useFrontCamera;
+    private boolean isDetecting; //cuma kirim new frame kalo yg terakhir udh selesai diproses
+    private int totalFrame, detectedFrame;
 
     private void toast(String s){
         Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
@@ -85,11 +93,11 @@ public class DetectionActivity extends AppCompatActivity {
         //bikin face detector nya
         detector = FirebaseVision.getInstance().getVisionFaceDetector(realTimeOptions);
 
-        //Siapin kamera
+        //region Siapin kamera
         cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
         try {
-            cameraId = cameraManager.getCameraIdList();
-            for (String id : cameraId){
+            cameraIds = cameraManager.getCameraIdList();
+            for (String id : cameraIds){
                 Log.d(TAG, "Camera Id: " + id);
             }
         }catch (CameraAccessException e){
@@ -108,29 +116,33 @@ public class DetectionActivity extends AppCompatActivity {
             @Override
             public void onDisconnected(@NonNull CameraDevice camera) {
                 Log.d(TAG, "Camera disconnected");
+                cameraDevice.close();
+                cameraDevice = null;
             }
 
             @Override
             public void onError(@NonNull CameraDevice camera, int error) {
                 Log.d(TAG, "Camera Error");
+                cameraDevice.close();
+                cameraDevice = null;
             }
         };
+        //endregion
 
-
-        //Siapin surface view (untuk preview kamera)
+        //region Siapin surface view (untuk preview kamera)
         surfaceView = findViewById(R.id.surfaceView);
         surfaceHolder = surfaceView.getHolder();
         surfaceHolderCallback = new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
                 Log.d(TAG, "surfaceCreated");
-                cameraSurface = holder.getSurface();
+                previewSurface = holder.getSurface();
             }
 
             @Override
             public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
                 Log.d(TAG, "surfaceChanged");
-                cameraSurface = holder.getSurface();
+                previewSurface = holder.getSurface();
                 surfaceReady = true;
                 startPreviewCamera();
             }
@@ -142,6 +154,29 @@ public class DetectionActivity extends AppCompatActivity {
             }
         };
         surfaceHolder.addCallback(surfaceHolderCallback);
+        //endregion
+
+        //Siapin imageReader
+        //width & height image yg beneran bakal ngikutin hasil preview kamera, kalo yg di set dsni lebih
+        //rendah malah lag gk jelas. Hopefully device dengan kamera resolusi tinggi punya processing power
+        //yang gede juga buat ngimbangin
+        imageReader = ImageReader.newInstance(3840, 2160, ImageFormat.YUV_420_888, 20);
+        detectionSurface = imageReader.getSurface();
+        imageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
+            @Override
+            public void onImageAvailable(ImageReader reader) {
+                Image image = reader.acquireLatestImage();
+                if(image != null){
+                    totalFrame++;
+                    if(isDetecting){
+                        image.close(); //buang frame
+                    }else{
+                        Log.d(TAG, "Image Size "  + image.getWidth() + " " + image.getHeight());
+                        parseFromCamera(image);
+                    }
+                }
+            }
+        }, new Handler());
     }
 
     @Override
@@ -155,11 +190,13 @@ public class DetectionActivity extends AppCompatActivity {
         }else{
             Log.d(TAG, "Camera permission granted");
             try {
-                if(useFrontCamera && cameraId.length > 1){
-                    cameraManager.openCamera(cameraId[1], cameraStateCallback, new Handler());
+                //kalo ternyata gak ada kamera depan, tetep pake yg belakang
+                if(useFrontCamera && cameraIds.length > 1){
+                    cameraId = cameraIds[1];
                 }else{
-                    cameraManager.openCamera(cameraId[0], cameraStateCallback, new Handler());
+                    cameraId = cameraIds[0];
                 }
+                cameraManager.openCamera(cameraId, cameraStateCallback, new Handler());
             }catch (CameraAccessException e){
                 toast("Error accessing camera");
                 e.printStackTrace();
@@ -197,6 +234,12 @@ public class DetectionActivity extends AppCompatActivity {
             cameraDevice.close();
             cameraDevice = null;
         }
+        imageReader.close();
+        try {
+            detector.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -208,7 +251,7 @@ public class DetectionActivity extends AppCompatActivity {
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
                 Log.d(TAG, "Camera permission granted");
                 try {
-                    cameraManager.openCamera(cameraId[0], cameraStateCallback, new Handler());
+                    cameraManager.openCamera(cameraIds[0], cameraStateCallback, new Handler());
                 }catch (CameraAccessException e){
                     toast("Error accessing camera");
                     e.printStackTrace();
@@ -225,8 +268,9 @@ public class DetectionActivity extends AppCompatActivity {
         //mulai preview kamera ke surface
         if(surfaceReady && cameraDevice != null && !isPreviewing){
             // prepare list of surfaces to be used in capture requests
-            List<Surface> sfl = new ArrayList<Surface>();
-            sfl.add(cameraSurface);
+            List<Surface> sfl = new ArrayList<>();
+            sfl.add(previewSurface);
+            sfl.add(detectionSurface);
             //Bikin capture session
             try{
                 cameraDevice.createCaptureSession(sfl, new CameraCaptureSession.StateCallback() {
@@ -237,7 +281,8 @@ public class DetectionActivity extends AppCompatActivity {
 
                         try{
                             CaptureRequest.Builder previewRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-                            previewRequestBuilder.addTarget(cameraSurface);
+                            previewRequestBuilder.addTarget(previewSurface);
+                            previewRequestBuilder.addTarget(detectionSurface);
                             captureSession.setRepeatingRequest(previewRequestBuilder.build(), null, null);
                         } catch (CameraAccessException e) {
                             e.printStackTrace();
@@ -257,24 +302,26 @@ public class DetectionActivity extends AppCompatActivity {
         }
     }
 
-    private void parseFromCamera(Image mediaImage, int rotation){
+    private void parseFromCamera(Image mediaImage){
+        isDetecting = true;
+        int rotation = getRotationCompensation(cameraId, this, this);
 
         image = FirebaseVisionImage.fromMediaImage(mediaImage, rotation);
+        mediaImage.close();
 
-        Task<List<FirebaseVisionFace>> result =
-                detector.detectInImage(image)
-                        .addOnSuccessListener(
-                                new OnSuccessListener<List<FirebaseVisionFace>>() {
+        detector.detectInImage(image).addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionFace>>() {
                                     @Override
                                     public void onSuccess(List<FirebaseVisionFace> faces) {
                                         processResult(faces);
+                                        isDetecting = false;
+                                        detectedFrame++;
+                                        Log.d(TAG, "Detection success " + detectedFrame + " from " + totalFrame);
                                     }
-                                })
-                        .addOnFailureListener(
-                                new OnFailureListener() {
+                                }).addOnFailureListener(new OnFailureListener() {
                                     @Override
                                     public void onFailure(@NonNull Exception e) {
                                         e.printStackTrace();
+                                        isDetecting = false;
                                     }
                                 });
     }
@@ -304,8 +351,7 @@ public class DetectionActivity extends AppCompatActivity {
      * Get the angle by which an image must be rotated given the device's current
      * orientation.
      */
-    private int getRotationCompensation(String cameraId, Activity activity, Context context)
-            throws CameraAccessException {
+    private int getRotationCompensation(String cameraId, Activity activity, Context context){
         // Get the device's current rotation relative to its "native" orientation.
         // Then, from the ORIENTATIONS table, look up the angle the image must be
         // rotated to compensate for the device's rotation.
@@ -316,9 +362,16 @@ public class DetectionActivity extends AppCompatActivity {
         // devices it is 270 degrees. For devices with a sensor orientation of
         // 270, rotate the image an additional 180 ((270 + 270) % 360) degrees.
         CameraManager cameraManager = (CameraManager) context.getSystemService(CAMERA_SERVICE);
-        int sensorOrientation = cameraManager
-                .getCameraCharacteristics(cameraId)
-                .get(CameraCharacteristics.SENSOR_ORIENTATION);
+        int sensorOrientation = 0;
+        try {
+            sensorOrientation = cameraManager
+                    .getCameraCharacteristics(cameraId)
+                    .get(CameraCharacteristics.SENSOR_ORIENTATION);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e){
+            e.printStackTrace();
+        }
         rotationCompensation = (rotationCompensation + sensorOrientation + 270) % 360;
 
         // Return the corresponding FirebaseVisionImageMetadata rotation value.
@@ -338,7 +391,7 @@ public class DetectionActivity extends AppCompatActivity {
                 break;
             default:
                 result = FirebaseVisionImageMetadata.ROTATION_0;
-                Log.e("Hmm", "Bad rotation value: " + rotationCompensation);
+                Log.e(TAG, "Bad rotation value: " + rotationCompensation);
         }
         return result;
     }
---
+++ app/src/main/java/com/interpixel/wibumeter_reloaded/PhotoDetectionActivity.java
@@ -0,0 +1,14 @@
+package com.interpixel.wibumeter_reloaded;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhotoDetectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_detection);
+    }
+}
